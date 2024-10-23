9ca8d9d89059751ebf37514bd9f10adf8aeb6130
Logesh <146101687+Logesh-irulappan@users.noreply.github.com>
2024-10-24 01:56:01 +0530

    Update Main.java

--- MineSweeper/Main.java
+++ MineSweeper/Main.java
@@ -4,9 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the Board Size: ");
-        int n = in.nextInt();
-        MineSweeper start = new MineSweeper(n);
+        System.out.println("Enter Level (int) 1. Easy    2. Medium    3. Hard  => ");
+        int level = in.nextInt();
+        int n = 0;
+        switch (level) {
+            case 1: {
+                n = 8;
+                break;
+            }
+            case 2: {
+                n = 12;
+                break;
+            }
+            case 3: {
+                n = 16;
+                break;
+            }
+        }
+        MineSweeper game = new MineSweeper(n);
     }
 
 }
