c40340f3543cd0819fdd01a9e5cb8b1acf71bc75
Logesh <146101687+Logesh-irulappan@users.noreply.github.com>
2024-11-15 13:07:01 +0530

    Updated GameBoard.java

--- MineSweeper/GameBoard.java
+++ MineSweeper/GameBoard.java
@@ -2,16 +2,16 @@ package MineSweeper;
 
 public class GameBoard {
 
-    private final int n;
+    private final int boardSize;
     private int flag;
-    private int[][] board;
-    private char[][] charBoard;
+    private int[][] internalBoard;
+    private char[][] displaySideBoard;
 
-    GameBoard(int n) {
-        this.n = n;
-        this.flag = n;
-        board = new int[n][n];
-        charBoard = new char[n][n];
+    GameBoard(int boardSize) {
+        this.boardSize = boardSize;
+        this.flag = boardSize;
+        internalBoard = new int[boardSize][boardSize];
+        displaySideBoard = new char[boardSize][boardSize];
         setCharBoard();
     }
 
@@ -19,125 +19,124 @@ public class GameBoard {
         return flag;
     }
 
-    public void initializeGameBoard(int r, int c) {
-        int bombCount = n;
+    public void initializeInternalGameBoard(int row, int col) {
+        int bombCount = boardSize;
         while(bombCount > 0) {
             // Getting random index for inserting bomb...
-            int x = (int) (Math.random() * n);
-            int y = (int) (Math.random() * n);
+            int x = (int) (Math.random() * boardSize);
+            int y = (int) (Math.random() * boardSize);
 
             // Neglecting the user's index position and inserting the bomb...
-            if( (x == r || x == (r-1) || x == (r+1)) && ( y== c || y == (c-1) || y == (c+1) ) ){
+            if( (x == row || x == (row -1) || x == (row +1)) && ( y== col || y == (col -1) || y == (col +1) ) ){
                 continue;
             }
-            else if(board[x][y] != -1) {
-                // -1 indicates the bomb in the board..
-                board[x][y] = -1;
+            else if(internalBoard[x][y] != -1) {
+                // -1 indicates the bomb in the internalBoard..
+                internalBoard[x][y] = -1;
                 bombCount--;
                 setAroundNumbers(x, y);
             }
         }
     }
 
-    public void setAroundNumbers(int r, int c) {
-        for(int i = -1; i <= 1; i++) {
-            for(int j = -1; j <= 1; j++) {
-                if( !isValidPosition(r+i, c+j) )
+    public void setAroundNumbers(int rowIndex, int colIndex) {
+        for(int row = -1; row <= 1; row++) {
+            for(int col = -1; col <= 1; col++) {
+                if( !isValidPosition(rowIndex +row, colIndex +col) )
                     continue;
-                else if( i == 0 && j == 0)
+                else if( row == 0 && col == 0)
                     continue;
-                else if( board[r+i][c+j] != -1 )
-                    board[r+i][c+j] += 1;
+                else if( internalBoard[rowIndex +row][colIndex +col] != -1 )
+                    internalBoard[rowIndex +row][colIndex +col] += 1;
             }
         }
     }
 
-    public boolean isValidPosition(int r, int c) {
-        return ( r > -1 && r < n ) && ( c > -1 && c < n );
+    public boolean isValidPosition(int row, int col) {
+        return ( row > -1 && row < boardSize) && ( col > -1 && col < boardSize);
     }
 
     // Actions
-
-    public void zeroBreak(int r, int c) {
-        // -9 representing the zero's at board array to break the cyclic recursion
-        board[r][c] = -9;
-        charBoard[r][c] = ' ';
+    public void zeroBreak(int rowIndex, int colIndex) {
+        // -9 representing the zero's at internalBoard array to break the cyclic recursion
+        internalBoard[rowIndex][colIndex] = -9;
+        displaySideBoard[rowIndex][colIndex] = ' ';
         // nested loop for accessing all 8 cells in 2D array
-        for(int i = -1; i <= 1; i++) {
-            for(int j = -1; j <= 1; j++) {
+        for(int row = -1; row <= 1; row++) {
+            for(int col = -1; col <= 1; col++) {
                 // checking for only valid index of our 2D array
-                if(isValidPosition(r+i, c+j)) {
+                if(isValidPosition(rowIndex +row, colIndex +col)) {
                     // Detecting number in surrounding 8 tiles...
-                    if (board[r + i][c + j] > 0) {
-                        charBoard[r + i][c + j] = String.valueOf(board[r + i][c + j]).charAt(0);
+                    if (internalBoard[rowIndex + row][colIndex + col] > 0) {
+                        displaySideBoard[rowIndex + row][colIndex + col] = String.valueOf(internalBoard[rowIndex + row][colIndex + col]).charAt(0);
                     }
                     // if zero is found repeat this function
-                    else if (board[r+i][c+j] == 0) {
-                        zeroBreak(r+i, c+j);
+                    else if (internalBoard[rowIndex +row][colIndex +col] == 0) {
+                        zeroBreak(rowIndex +row, colIndex +col);
                     }
                 }
             }
         }
     }
 
-    public boolean isBomb(int r, int c) {
-        return board[r][c] == -1;
+    public boolean isBomb(int row, int col) {
+        return internalBoard[row][col] == -1;
     }
 
 
     // After Player losing the game, the wrongly placed flags and actual bomb places are opened..
     public void openAllBomb() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int row = 0; row < boardSize; row++) {
+            for (int col = 0; col < boardSize; col++) {
                 // wrong flag placed mark
-                if( board[i][j] != -1 && charBoard[i][j] == 'F' )
-                    charBoard[i][j] = '/';
+                if( internalBoard[row][col] != -1 && displaySideBoard[row][col] == 'F' )
+                    displaySideBoard[row][col] = '/';
                 // bomb places
-                else if( board[i][j] == -1 ) {
-                    charBoard[i][j] = 'B';
+                else if( internalBoard[row][col] == -1 ) {
+                    displaySideBoard[row][col] = 'B';
                 }
             }
         }
     }
 
-    public void userMove(int r, int c) {
-        if(charBoard[r][c] != '*') {
+    public void userMove(int row, int col) {
+        if(displaySideBoard[row][col] != '*') {
             System.out.println("This Cell is Already opened");
         }
-        else if(board[r][c] == 0) {
-            zeroBreak(r, c);
+        else if(internalBoard[row][col] == 0) {
+            zeroBreak(row, col);
         }
         else {
-            charBoard[r][c] = String.valueOf(board[r][c]).charAt(0);
+            displaySideBoard[row][col] = String.valueOf(internalBoard[row][col]).charAt(0);
         }
     }
 
-    public void setFlag(int r, int c) {
-        if(charBoard[r][c] != 'F' && charBoard[r][c] != '*') {
+    public void setFlag(int row, int col) {
+        if(displaySideBoard[row][col] != 'F' && displaySideBoard[row][col] != '*') {
             System.out.println("Index is Already Opened, Flag cannot be inserted.");
         }
-        else if(charBoard[r][c] == 'F') {
-            charBoard[r][c] = '*';
+        else if(displaySideBoard[row][col] == 'F') {
+            displaySideBoard[row][col] = '*';
             flag += 1;
         }
         else {
-            charBoard[r][c] = 'F';
+            displaySideBoard[row][col] = 'F';
             flag -= 1;
         }
     }
 
     // If an unopened index is flag by user, User has to unFlag it first, inorder to open that index.
-    public boolean isFlagged(int r, int c) {
-        return charBoard[r][c] == 'F';
+    public boolean isFlagged(int row, int col) {
+        return displaySideBoard[row][col] == 'F';
     }
 
     // player winning check, only when all flags are placed
     public boolean winCheck() {
         if( !isAllCellsOpened() )
             return false;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if( board[i][j] == -1 && charBoard[i][j] != 'F' )
+        for(int row = 0; row < boardSize; row++) {
+            for(int col = 0; col < boardSize; col++) {
+                if( internalBoard[row][col] == -1 && displaySideBoard[row][col] != 'F' )
                     return false;
             }
         }
@@ -147,34 +146,34 @@ public class GameBoard {
     // checking for Unopened cells before going into win checking..
     public boolean isAllCellsOpened() {
         int breakCount = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if( board[i][j] == -1 )
+        for(int row = 0; row < boardSize; row++) {
+            for(int col = 0; col < boardSize; col++) {
+                if( internalBoard[row][col] == -1 )
                     continue;
-                else if( charBoard[i][j] == '*' )
+                else if( displaySideBoard[row][col] == '*' )
                     return false;
             }
         }
         return true;
     }
 
-    // Displaying and setting the board to initial stage
+    // Displaying and setting the internalBoard to initial stage
     public void setCharBoard() {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++)
-                charBoard[i][j] = '*';
+        for(int row = 0; row < boardSize; row++) {
+            for(int col = 0; col < boardSize; col++)
+                displaySideBoard[row][col] = '*';
         }
     }
 
     public void displayCharBoard() {
         System.out.print(" ");
-        for (int i = 0; i < n; i++)
-            System.out.print("  "+i);
+        for (int row = 0; row < boardSize; row++)
+            System.out.print("  "+row);
         System.out.println();
-        for(int i = 0; i < n; i++) {
-            System.out.print(i+"  ");
-            for(int j = 0; j < n; j++)
-                System.out.print(charBoard[i][j]+"  ");
+        for(int row = 0; row < boardSize; row++) {
+            System.out.print(row+"  ");
+            for(int col = 0; col < boardSize; col++)
+                System.out.print(displaySideBoard[row][col]+"  ");
             System.out.println();
         }
         System.out.println();
